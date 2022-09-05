d8ba01c365d4bfc6ee4f7d6f76a24f224f85aef2
rondolphstm <rondolphtst.m@gmail.com>
2022-09-05 19:45:41 -0400

    fixed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import Menubar from './components/Menubar'
+import Hero from './components/Hero'
+import Flipcards from './components/Flipcards'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+   <Flipcards/>
+   <p>hello</p>
+   <Menubar/>
+   <Hero/>
+   </>
   );
 }
 
---
+++ src/components/Menubar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+
+export default function MenuBar() {
+  return (
+    <>
+     {/* <ul>
+  <li><a href="#" target = "_blank">Home</a></li>
+  <li><a href="#">News</a></li>
+  <li><a href="#">Contact</a></li>
+  <li><a href="#">About</a></li>
+</ul> */}
+    </>
+  )
+}
+
+
